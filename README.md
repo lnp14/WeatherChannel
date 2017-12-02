f39112f11028bee5cf78c249fa0b7851988e4d9d
Lauren Perlmutter <lnp14@my.fsu.edu>
2017-12-02 00:10:17 +0000

    Extra Credit final commit

--- README.md
+++ README.md
@@ -4,19 +4,19 @@
 Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+December 1, 2017
 
 ### Student Name:  
-(First and Last Name)
+Lauren Perlmutter
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+In this extra credit assignment, we focused on enhancing an API-centric web application that will create Seminole Weather. We utilized Apixu API to allow users to obtain weather data for developing weather apps. We first had to create an account with Apixu API to obtain an API key. Then, we had to input the API key into the url code. Next, we needed to create additional rows in to display the weather for days 4-10 and change the number of weather forecast days to 10 from 3. Next, we added code to indicate where the weather data obtained for days 4-10. Finally, to complete the application we added the code to pull out of the HTTP Request the forecast weather data for days 4-10.
 
 ### View Project:
-(Replace this statement with your Github Page URL that was created when you 
- published the project.)
+https://lnp14.github.io/WeatherChannel/
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
+1. A concept I learned in this lesson is what an API is. An application programming interface key is a code passed in by computer programs calling an API to identify the calling program, its developer, or its user to the Web site. These keys are used to track and control how it is being used.
+2. A concept I learned in this lesson is what an HTTP Request consists of and its function in this application. An HTTP Request to the Apixu API server consists of the base URL and the API method. This API allows both HTTP and HTTPS requests. The HTTP Request here in this application is the location where the user specifies and will retrieve certain data based on their entry.
+3. A concept I learned in this lesson is an additional use for the (.done) function. We have utilized this function in the past, but we used it once again to indicate where the weather data retrieved for days 4-10 will be placed on the HTML page. This function allowed us to expand the application from a 3-day weather forecast to a 10-day weather forecast application.
+
--- apixu_forecast.js
+++ apixu_forecast.js
@@ -1,8 +1,8 @@
 /*
-    Program Name:   Seminole Weather Channel
-    Author: 
-    Date: 
-    Filename:   apixu_forecast.js
+    Program Name: Seminole Weather Channel
+    Author: Lauren Perlmutter
+    Date: December 1, 2017
+    Filename: apixu_forecast.js
 */
 
  /*global $*/ 
@@ -87,10 +87,10 @@
 
         $(document).ready(function(){
            $(':button').click(function(){
-                var message = 'q=' + $('#searchValue').val()+'&days=3';   
+                var message = 'q=' + $('#searchValue').val()+'&days=10';   
                 $.ajax({
                       type: 'GET',
-                      url: 'https://api.apixu.com/v1/forecast.json?key=ADD_YOUR_API_KEY_HERE',
+                      url: 'https://api.apixu.com/v1/forecast.json?key=2ac6909a8c7f4858a0c232638170112',
                       data: message             
                 })
                 .done(function(json){
@@ -102,9 +102,24 @@
                      $('#Day1High').html("<center>" + json.forecast.forecastday[0].day.maxtemp_f + "</center>");
                      $('#Day2Low').html("<center>" + json.forecast.forecastday[1].day.mintemp_f + "</center>");
                      $('#Day2High').html("<center>" + json.forecast.forecastday[1].day.maxtemp_f + "</center>");        
-
-                     
+                     $('#Day3Low').html("<center>" + json.forecast.forecastday[2].day.mintemp_f + "</center>");
+                     $('#Day3High').html("<center>" + json.forecast.forecastday[2].day.maxtemp_f + "</center>");  
+                     $('#Day4Low').html("<center>" + json.forecast.forecastday[3].day.mintemp_f + "</center>");
+                     $('#Day4High').html("<center>" + json.forecast.forecastday[3].day.maxtemp_f + "</center>");  
+                     $('#Day5Low').html("<center>" + json.forecast.forecastday[4].day.mintemp_f + "</center>");
+                     $('#Day5High').html("<center>" + json.forecast.forecastday[4].day.maxtemp_f + "</center>"); 
+                     $('#Day6Low').html("<center>" + json.forecast.forecastday[5].day.mintemp_f + "</center>");
+                     $('#Day6High').html("<center>" + json.forecast.forecastday[5].day.maxtemp_f + "</center>"); 
+                     $('#Day7Low').html("<center>" + json.forecast.forecastday[6].day.mintemp_f + "</center>");
+                     $('#Day7High').html("<center>" + json.forecast.forecastday[6].day.maxtemp_f + "</center>"); 
+                     $('#Day8Low').html("<center>" + json.forecast.forecastday[7].day.mintemp_f + "</center>");
+                     $('#Day8High').html("<center>" + json.forecast.forecastday[7].day.maxtemp_f + "</center>"); 
+                     $('#Day9Low').html("<center>" + json.forecast.forecastday[8].day.mintemp_f + "</center>");
+                     $('#Day9High').html("<center>" + json.forecast.forecastday[8].day.maxtemp_f + "</center>"); 
+                     $('#Day10Low').html("<center>" + json.forecast.forecastday[9].day.mintemp_f + "</center>");
+                     $('#Day10High').html("<center>" + json.forecast.forecastday[9].day.maxtemp_f + "</center>"); 
                 })
+                
                 .fail(function() {
                       alert("Posting failed.");              
                 });                    
